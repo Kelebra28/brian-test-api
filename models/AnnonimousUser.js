a6f06231561a0b32248032ee9329c51dd45725d2
Kelebra <ricardokelebra@gmail.com>
2023-02-04 21:06:53 -0600

    feature: create new schema annonimos user

--- models/AnnonimousUser.js
+++ models/AnnonimousUser.js
@@ -5,8 +5,66 @@ const AnnonimousUserSchema = mongoose.Schema({
         type: String,
         required :  true
     },
-    test : {
+    tests : [{
+            name: {
+                type: String,
+                required: true
+            },
+            interSearch: {
+                type: Number,
+                required: true
+            },
+            interPerfection: {
+                type: Number,
+                required: true
+            },
+            interCondemnation: {
+                type: Number,
+                required: true
+            },
+            interBTF: {
+                type: Number,
+                required: true
+            },
+            interVictimization: {
+                type: Number,
+                required: true
+            },
+            interAnxious: {
+                type: Number,
+                required: true
+            },
+            interAvoidant: {
+                type: Number,
+                required: true
+            },
+            interInsecure: {
+                type: Number,
+                required: true
+            },
+            interPast:  {
+                type: Number,
+                required: true
+            },
+            interProcrastination:  {
+                type: Number,
+                required: true
+            },
+    }],
+    frist_name: {
         type: String,
+        required: true,
+    },
+    last_name: {
+        type: String,
+        required: true,
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
         required: true
     },
     date: {
@@ -20,6 +78,7 @@ const AnnonimousUserSchema = mongoose.Schema({
     },
     contacted: {
         type: Boolean,
+        required: true,
         default: false
     },
 });
